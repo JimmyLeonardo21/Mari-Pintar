e910a9b23423d95e072c1a7194bac186a71f9d13
Mahardikadwiputra27 <mahardikadwiputra27@gmail.com>
2022-01-26 20:39:00 +0700

    1

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
+const router = require('./routes/index')
 const port = 3000
 
 
+app.use(express.urlencoded({ extended: true }))
+app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+app.use('/', router)
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
