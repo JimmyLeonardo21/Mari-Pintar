aa3323b7d8c55f2ea8bebee85baa70279a8311bd
Jimmylee <jimmy.leonardo21@gmail.com>
2022-01-27 22:54:14 +0700

    nambar fitur middleware dan i session

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const express = require('express')
 const app = express()
 const router = require('./routes/index')
+const session = require('express-session')
 const port = 3000
-
 
 app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
 
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: false,
+    sameSite: true
+   }
+}))
 app.use('/', router)
+
+router.use(function (req, res, next) {
+  console.log(req.session, "<><><><>ini yg di app<>");
+  console.log('Time:', Date.now())
+  next()
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
