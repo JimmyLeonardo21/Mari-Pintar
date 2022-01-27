0cb62c30629d30f800e78eabdd2a19c3c9c05706
Mahardikadwiputra27 <mahardikadwiputra27@gmail.com>
2022-01-27 21:20:32 +0700

    search

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,13 +1,29 @@
 
 
-const {Course} = require('../models')
+const {Course, Category} = require('../models')
 const getYoutube = require('../helpers/getYoutube')
+const user = require('../models/user')
+const { Op } = require('sequelize')
 
 class Controller{
 
     static findAllCourses(req, res){
-        Course.findAll()
+        let options = {include : Category}
+        if (req.query){
+            console.log(req.query)
+            options.where = {}
+            if (req.query.name) {
+                options.where.name = {
+                    [Op.iLike]: `${req.query.name}%`
+                }
+
+            }
+        }
+        Course.findAll(options)
         .then(data =>{
+        data.forEach(el =>{
+            console.log(el.Category.name)
+        })
             res.render('landing', {data})
         })
         .catch(err =>{
@@ -15,7 +31,8 @@ class Controller{
         })
     }
     static doCourse(req, res){
-        let id = req.params.id 
+        let id = req.params.id
+       
         Course.findByPk(id)
         .then(data =>{
             let link = getYoutube
