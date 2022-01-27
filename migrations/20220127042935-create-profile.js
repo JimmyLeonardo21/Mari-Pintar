ed9569909e317440648b6ccdd03634df45a4bc7d
Mahardikadwiputra27 <mahardikadwiputra27@gmail.com>
2022-01-27 11:41:58 +0700

    migrate/seed

---
+++ migrations/20220127042935-create-profile.js
@@ -0,0 +1,43 @@
+'use strict';
+module.exports = {
+  up:  (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Profiles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      gender: {
+        type: Sequelize.STRING
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+      UserId: {
+        type: Sequelize.INTEGER,
+        references:{
+          model: 'Users',
+          key: 'id'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down:  (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Profiles');
+  }
+};
---
+++ seeders/20220127043802-seedCategories.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+  up:  (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   const fs = require('fs')
+   let data = JSON.parse(fs.readFileSync('./data/categories.json', 'utf-8'))
+   data.forEach(el => {
+     el.createdAt = new Date()
+     el.updatedAt = new Date()
+   });
+   return queryInterface.bulkInsert('Categories', data, {})
+  },
+
+  down:  (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+     return queryInterface.bulkDelete('Categories', null, {})
+  }
+};
